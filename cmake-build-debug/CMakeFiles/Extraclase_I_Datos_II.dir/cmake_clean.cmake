02db19e68c5f71dbab942bf356896a87d065c7a7
Jose Alejandro <joalchama@gmail.com>
2020-02-24 20:50:15 -0600

    Hello World

---
+++ cmake-build-debug/CMakeFiles/Extraclase_I_Datos_II.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Extraclase_I_Datos_II.dir/main.cpp.o"
+  "Extraclase_I_Datos_II"
+  "Extraclase_I_Datos_II.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Extraclase_I_Datos_II.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Extraclase_I_Datos_II.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Extraclase_I_Datos_II.
+# This may be replaced when dependencies are built.
